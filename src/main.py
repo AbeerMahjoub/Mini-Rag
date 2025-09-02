fda65b0446cd49e6001d900013e6a3283e7a435a
Abeer Mahjoub <beroadel23@gmail.com>
2025-09-02 16:28:54 +0300

    schema

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from routes import base
 from routes import data
-
+from motor.motor_asyncio import AsyncMetorClient
+from helpers.config import get_settings
 
 
 
 app = FastAPI()
+
+@app.onevent('startup')
+async def startup_db_client():
+    settings = get_settings()
+
+    app.mongo_conn = AsyncMetorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+
+@app.onevent('shutdown')
+async def shutdown_db_client():
+    app.mongo_conn.close()
+
 
 app.include_router(base.base_router)
 
