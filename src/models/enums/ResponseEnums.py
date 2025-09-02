18ab46e4d322bb763927b1ad6ac162512d76191a
Abeer Mahjoub <beroadel23@gmail.com>
2025-09-02 12:01:17 +0300

    processing file

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -5,5 +5,7 @@ class ResponseSignals(Enum):
     FILE_SIZE_EXCEEDED = "file size exceeded"
     FILE_UPLOAD_SUCCESS = 'file upload success'
     FILE_UPLOAD_FAILED = 'file upload failed'
+    PROCESSING_SUCCESS = 'processing success'
+    PROCESSING_FAILED = "processing failed"
 
     
--- src/routes/data.py
+++ src/routes/data.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
 import os
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 from models import ResponseSignals
 import aiofiles
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger("uvicorn.error")
  
@@ -32,7 +33,7 @@ async def upload_data(project_id: str, file: UploadFile, app_settings: Settings 
     
 
     project_dir_path = ProjectController().get_project_path(project_id= project_id)
-    file_path = data_controller.generate_unique_filename(orig_file_name= file.filename,
+    file_path, file_id = data_controller.generate_unique_filepath(orig_file_name= file.filename,
                                                          project_id= project_id)
 
     try:
@@ -41,7 +42,8 @@ async def upload_data(project_id: str, file: UploadFile, app_settings: Settings 
                 await f.write(chunk)
         return JSONResponse(
                 content = {
-                    'signal': ResponseSignals.FILE_UPLOAD_SUCCESS.value
+                    'signal': ResponseSignals.FILE_UPLOAD_SUCCESS.value,
+                    'file_id': file_id
                 }
             )
         
@@ -52,12 +54,34 @@ async def upload_data(project_id: str, file: UploadFile, app_settings: Settings 
         return JSONResponse(
             status_code = status.HTTP_400_BAD_REQUEST,
             content = {
-                'signal': ResponseSignals.FILE_UPLOAD_FAILED.value
+                'signal': ResponseSignals.FILE_UPLOAD_FAILED.value,
+                
             }
         )
 
 
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+    process_controller = ProcessController(project_id= project_id)
 
+    file_content = process_controller.get_file_content(file_id= file_id)
+    file_chunks = process_controller.process_file_content(file_content= file_content,
+                                                          file_id= file_id,
+                                                          chunk_size= chunk_size,
+                                                          overlap_size= overlap_size)
+    
+    if file_chunks is None or len(file_chunks) ==0:
+        return JSONResponse(
+                content = {
+                    'signal': ResponseSignals.PROCESSING_FAILED.value,}
+                    
+            )
+
+    
+    return file_chunks
         
 
 
